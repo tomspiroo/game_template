d8689ef5276f0ac05815a0424e11407e5e4aeebc
Tom <tspiritosanto@mawsonrovers.com>
2024-07-17 14:28:35 +1000

    Added grass graphics and support file for cvs and data manipulation

--- src/level.py
+++ src/level.py
@@ -5,6 +5,8 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import *
+from random import choice
 
 class Level:
     def __init__(self):
@@ -20,10 +22,30 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        # for row_index, row in enumerate(WORLD_MAP):
-        #     for col_index, col in enumerate(row):
-        #         x = col_index * TILESIZE
-        #         y = row_index * TILESIZE
+        layouts = {
+            'boundary': import_csv_layout('../map/map_FloorBlocks.csv'),
+            'grass': import_csv_layout('../map/map_Grass.csv'),
+            'object': import_csv_layout('../map/map_LargeObjects.csv'),
+
+        }
+        graphics = {
+            'grass': import_folder('../graphics/grass')
+        }
+
+        for style, layout in layouts.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x,y),[self.obstacle_sprites],'invisible')
+                        if style == 'grass':
+                            random_grass_image = choice(graphics['grass'])
+                            Tile((x,y),[self.visible_sprites],'grass',random_grass_image)
+                        if style == 'object':
+                            pass
+                        
         #         if col == 'x':
         #             Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
         #         if col == 'p':

599aa504bbd17bbd82a574c3f4aa8aea13e4fc48
Tom <tspiritosanto@mawsonrovers.com>
2024-07-17 11:22:20 +1000

    Added hitbox & overlaps

--- src/level.py
+++ src/level.py
@@ -1,4 +1,6 @@
+from typing import Iterable
 import pygame
+from pygame.sprite import AbstractGroup
 from settings import *
 from tile import Tile
 from player import Player
@@ -11,7 +13,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         # sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         # sprite setup
@@ -29,6 +31,25 @@ class Level:
 
     def run(self):
         # update and draw game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        # general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self,player):
+
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+            offset_position = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_position)
--- src/tile.py
+++ src/tile.py
@@ -6,3 +6,4 @@ class Tile(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0,-10) # x and y args, whatever the value it will make it shrink by (value)/2 on each side
